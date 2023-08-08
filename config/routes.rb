9f76e52df71093f9a2dc5ec36bde4021aa5f6ae5
Caitlyn Khoo <caitlynkhoo@gmail.com>
2023-08-08 16:30:42 +1000

    completed to do list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # # Read all
+  # get '/tasks', to: 'tasks#index', as: :tasks
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # # Create -> 2 steps: form -> create action
+  # # form (new page)
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
+  # # create
+  # post '/tasks', to: 'tasks#create' # the prefix is tasks
+
+  # # Read one
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # # Update
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # Delete
+  # delete '/tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
